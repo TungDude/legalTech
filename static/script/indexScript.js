e8e182ca071721b2c50925061075818206862d92
Tung <krittapop.kms@gmail.com>
2024-01-06 22:06:36 +0700

    remove method 1 calculation

--- static/script/indexScript.js
+++ static/script/indexScript.js
@@ -1,40 +1,29 @@
 async function methodInput() {
     const method = document.getElementById('method').value;
     var container = document.getElementById('parameterContainer');
-    let CTT = document.getElementById("CTT_default");
-
-    try {
-        var CTT_2 = document.getElementById("CTT_2");
-        CTT_2.textContent = "";
-    }
-    catch (Exception){
-        console.log(Exception);
-    }
-
-    CTT.textContent = "";
 
     if (method == "1") {
-        container.innerHTML = '<label for="T">จำนวนต้นไม้ในพื้นที่โครงการทั้งหมด (ต้น): </label><input type="number" id="T" name="T"><label for="t">ปีที่ดำเนินการติดตามผล (ปี) : </label><input type="number" id="t" name="t"><label for="MAI"> อัตราการเพิ่มพูนปริมาณการกักเก็บคาร์บอนของต้นไม้ (kgCO2/ต้น/ปี): </label><input type="number" id="MAI" name="MAI"><br><button class="calculateButton" id="calculate" onclick="calculateCTT()">Calculate</button>';
+        container.innerHTML = '<form action="/method1" method="post" enctype="multipart/form-data"><label for="T">จำนวนต้นไม้ในพื้นที่โครงการทั้งหมด (ต้น): </label><input type="number" id="T" name="T"><label for="t">ปีที่ดำเนินการติดตามผล (ปี) : </label><input type="number" id="t" name="t"><label for="MAI"> อัตราการเพิ่มพูนปริมาณการกักเก็บคาร์บอนของต้นไม้ (kgCO2/ต้น/ปี): </label><input type="number" id="MAI" name="MAI"><br><input class="calculateButton" type="submit" value="Calculate"></form>';
     }
 
     if (method == "2") {
-        container.innerHTML = '<h1>Upload Excel File</h1><form action="/upload" method="post" enctype="multipart/form-data"><input type="file" name="file"><input class="uploadButton" type="submit" value="Upload"></form>';
+        container.innerHTML = '<h1>Upload Excel File</h1><form action="/method2" method="post" enctype="multipart/form-data"><input type="file" name="file"><input class="uploadButton" type="submit" value="Upload"></form>';
     }
 }
 
-async function calculateCTT() {
-    const method = document.getElementById('method').value;
-    var CTT = document.getElementById("CTT_default");
+// async function calculateCTT() {
+//     const method = document.getElementById('method').value;
+//     var CTT = document.getElementById("CTT_default");
 
-    if (method == "1") {
-        const t = document.getElementById('t').value;
-        const T = document.getElementById('T').value;
-        const MAI = document.getElementById('MAI').value;
+//     if (method == "1") {
+//         const t = document.getElementById('t').value;
+//         const T = document.getElementById('T').value;
+//         const MAI = document.getElementById('MAI').value;
 
-        CTT.textContent = "CTT: " + (t * T * MAI * 0.001).toString();
-    }
+//         CTT.textContent = "CTT: " + (t * T * MAI * 0.001).toString();
+//     }
 
-    else {
-        CTT.textContent = "";
-    }
-}
+//     else {
+//         CTT.textContent = "";
+//     }
+// }
